5a63f11103ec8b842a37fbde129233de42536b8a
smuralee <suraj@surajmuraleedharan.com>
2022-12-25 17:56:52 +0000

    Fixed copyright header

--- src/main/java/com/smuralee/documents/types/PowerpointFileType.java
+++ src/main/java/com/smuralee/documents/types/PowerpointFileType.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2018 Suraj Muraleedharan
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.smuralee.documents.types;
 
 import com.smuralee.documents.dto.FileInfo;
@@ -9,7 +25,7 @@ import java.io.File;
 import java.io.IOException;
 
 /**
- * @author Suraj Muraleedharan (smuralee)
+ * @author Suraj Muraleedharan
  */
 public class PowerpointFileType extends AbstractFileType {
 
